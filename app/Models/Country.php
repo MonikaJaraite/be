b0a4abf486858c66e155b32625391a762240dbd5
MonikaJaraite <monjara@gmail.com>
2023-05-19 13:34:27 +0300

    Initial commit

---
+++ app/Models/Country.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'season_start', 'season_end'];
+    public $timestamps = false;
+}
---
+++ app/Models/Photo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $fillable = ['url', 'hotel_id'];
+    public $timestamps = false;
+}
